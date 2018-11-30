65a274364984fa6b9fe7914147843697c5679663
Elevi <elevisc@gmail.com>
2018-11-29 19:23:05 -0800

    First implementation of encrypt function working

--- encrypt.cpp
+++ encrypt.cpp
@@ -1,37 +1,93 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
-char encrypt(int, int);
+void encrypt(int, string);
+void print(vector<char>&);
+int getLetterValue(string);
 
 int main() {
-  queue<char> alphabet;
-  // Create an array of chars that contain the alphabet
   vector<char> str;
   // A starts at 65
   // Z ends at 90
   for (int i = 65; i <= 90; i++) {
     // cout << char(i) << endl;
     str.push_back(char(i));
+    //cout << char(i) << endl;
   }
 
-  //encrypt(2, 1);
-
+  encrypt(6, "K");
 }
 
-void encrypt(int shift, int keyShift) {
-  queue<char> alphabet;
+/*void encryptLoop(string plainText, string keyWord) {
+  int plainTextLength = plainText.size();
+  int keyWordLength = keyWord.size();
+  // Check that keyword is at least as long as plain text
+  if (plainTextLength < keyWordLength) {
 
-  // Generate the alphabet
-  for (int i = 65; i <= 90; i++) {
-    cout << char((i + shift) % 65 + 65) << endl;
-    alphabet.push(char((i + shift) % 91));
   }
-  
-  /*for (int i = 1; i <= keyShift; i++) {
-    alphabet.pop();
+  // Container for our alphabet
+  vector<char> alphabet;
+
+  // Generate the alphabet that is shifted 
+  for (int i = 65 + shift; i < 91 + shift; i++) {
+    // If we pass the letter 'Z', start over
+    if ((i % 91) < 65) {
+      //cout << char((i % 91) + 65) << endl;
+      alphabet.push_back(char((i % 91) + 65));
+    }
+    // Push back into our array like normal
+    else {
+      //cout << char(i) << endl;
+      alphabet.push_back(char(i));
+    }
   }
-  cout << alphabet.front() << endl;*/
+}
+
+// Function to extend keyword if it is not as long as plainText
+string extendKeyWord(string keyWord, int plainTextLength) {
+  // Declare empty container for output
+  string extendedKeyWord = "";
+  for (int i = 0; i < plainTextLength; i++) {
+    for (int j = 0; i < keyWord.size(); j++) {
+
+    }
+  }
+}*/
+void encrypt(int shift, string letter) {
+  // Container for our alphabet
+  vector<char> alphabet;
+
+  // Generate the alphabet that is shifted 
+  for (int i = 65 + shift; i < 91 + shift; i++) {
+    // If we pass the letter 'Z', start over
+    if ((i % 91) < 65) {
+      //cout << char((i % 91) + 65) << endl;
+      alphabet.push_back(char((i % 91) + 65));
+    }
+    // Push back into our array like normal
+    else {
+      //cout << char(i) << endl;
+      alphabet.push_back(char(i));
+    }
+  }
+
+  // Output alphabet to test
+  print(alphabet);
+
+  //
+  cout << "\nEncrypted letter: " << alphabet[getLetterValue(letter)] << endl;
+}
+
+int getLetterValue(string letter) {
+  int letterValue = (letter[0] - 'A');
+  return letterValue;
+} 
+
+void print(vector<char> &az) {
+  // Iterate through vector and print out value
+  for (vector<char>::iterator it = az.begin(); it != az.end(); it++) {
+    cout << *it << " ";
+  }
 }
